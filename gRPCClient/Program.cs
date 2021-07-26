c33c5df1b0aee906f173e75f5e0ad394e322ae7f
gkbrk7 <gokberky1996@gmail.com>
2021-07-27 01:39:28 +0300

    Add Products Service

--- gRPCClient/Program.cs
+++ gRPCClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Grpc.Net.Client;
 using gRPCServer;
+using gRPCServer.Protos;
 using System;
 using System.Threading.Tasks;
 
@@ -9,11 +10,29 @@ namespace gRPCClient
     {
         static async Task Main(string[] args)
         {
-            var data = new HelloRequest { Name = "Gokberk" };
+            //var data = new HelloRequest { Name = "Gokberk" };
+            //var grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(grpcChannel);
+            //var response = await client.SayHelloAsync(data);
+            //Console.WriteLine(response.Message);
+            //Console.ReadLine();
+
+
+            // Products
+            var data = new GetProductByIdModel { ProductId = 2 };
             var grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Greeter.GreeterClient(grpcChannel);
-            var response = await client.SayHelloAsync(data);
-            Console.WriteLine(response.Message);
+            var client = new Product.ProductClient(grpcChannel);
+            var response = await client.GetProductByIdAsync(data);
+            Console.WriteLine(response);
+            Console.ReadLine();
+            using (var clientData = client.GetAllProducts(new GetAllProductsRequest()))
+            {
+                while (await clientData.ResponseStream.MoveNext(new System.Threading.CancellationToken()))
+                {
+                    var thisProduct = clientData.ResponseStream.Current;
+                    Console.WriteLine(thisProduct);
+                }
+            }
             Console.ReadLine();
         }
     }
