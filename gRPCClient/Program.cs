0eeb7556edae6497ed654bf026fc49327452d5a1
gkbrk7 <gokberky1996@gmail.com>
2021-07-27 01:22:20 +0300

    Add project files.

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using gRPCServer;
+using System;
+using System.Threading.Tasks;
+
+namespace gRPCClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var data = new HelloRequest { Name = "Gokberk" };
+            var grpcChannel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(grpcChannel);
+            var response = await client.SayHelloAsync(data);
+            Console.WriteLine(response.Message);
+            Console.ReadLine();
+        }
+    }
+}
